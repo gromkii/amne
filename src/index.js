1986699d7fa20d8968f13a7ff1fae707c141e0d6
Dax <limeside@gmail.com>
2017-08-10 21:19:00 -0500

    Refactor index, data parses as a func. :rabbit:

--- src/index.js
+++ src/index.js
@@ -1,24 +1,62 @@
 (function(){
   var fs = require('fs');
 
+  /**
+   * Input from CLI, hopefully the input.txt path.
+   * @type {String}
+   */
   var input = process.argv[2] ? process.argv[2] : null;
 
-  if (input && process.argv.length === 3) {
-    fs.readFile(input, (err, data) => {
-      if (err) {
-        console.log('Error: ' + error);
-      }
+  /**
+   * Reading the data from input and converting to a sting.
+   * @type {String}
+   */
+  var inputData = fs.readFileSync(input, 'utf8').toString();
 
-      var formatData = String(data).split('\n');
-      var args = formatData[0];
-      var inputData = formatData[1];
+  /**
+   * Get objet of parsed data.
+   * @type {Object}
+   */
+  var parsed = parseData(inputData);
 
-      console.log('Arguments: ' + args);
-      console.log('Supplied data: ' + inputData);
-     });
-  } else {
+  /**
+   * Abstract parsed data into variables.
+   * @type {Int, Int, Array}
+   */
+  var {
+    days,
+    dayWindow,
+    priceRange
+  } = parsed;
+
+  console.log(days);
+  console.log(dayWindow);
+  console.log(priceRange);
+
+  /**
+   * parseData from inputData
+   * @param  {String} data
+   * @return {Object}      An object containing parsed input data.
+   */
+  function parseData(data) {
+    var formatData = data.split('\n');
+    var args       = formatData[0].split(' ');
+    var days       = parseInt(args[0]);
+    var dayWindow  = parseInt(args[1]);
+    var priceRange = formatData[1];
+
+    return {
+      days,
+      dayWindow,
+      priceRange
+    }
+  }
+
+  function createSubranges(days, dayWindow, priceRange) {
 
   }
+
+
 
 
 }());
